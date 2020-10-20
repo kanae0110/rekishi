78629f2b1b30b94db5380a9d907bf835486f4e3b
EC2 Default User <ec2-user@ip-172-31-33-217.us-east-2.compute.internal>
2020-10-20 13:49:53 +0000

    ストーリーインデックス

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
 Rails.application.routes.draw do
- get "/quizzes", to: "quizzes#index"
- post "/quizzes", to: "quizzes#create"
- get "/quizzes/new", to: "quizzes#new", as: "new_quiz"
- get "/quizzes/:id/edit", to: "quizzes#edit", as: "edit_quiz"
- get "/quizzes/:id", to: "quizzes#show", as: "quiz"
- patch "/quizzes/:id", to: "quizzes#update"
- delete "/quizzes/:id", to: "quizzes#destroy"
+ get "/stories", to: "stories#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,5 +2,10 @@ Category.create(name: "事件", period: "江戸末期")
 Category.create(name: "人物", period: "江戸末期")
 
 SubCategory.create(category_id: 1, name: "黒船来航")
-Quiz.create(sub_category_id: 2, question: "龍馬が暗殺された場所はどこか？", answer1: "寺田屋", answer2: "近江屋", answer3: "池田屋", answer: "近江屋", description: "慶応三年１１月１５日近江屋にて暗殺"
+Quiz.create(sub_category_id: 2, question: "龍馬が暗殺された場所はどこか？", answer1: "寺田屋", answer2: "近江屋", answer3: "池田屋", answer: "近江屋", discription: "慶応三年１１月１５日近江屋にて暗殺"
 )
+
+Story.create(name: "龍馬の生い立ち", category: "人物")
+Story.create(name: "とある貴族の幕末京都", category: "人物")
+Story.create(name: "天誅", category: "事件")
+Story.create(name: "京の戦い", category: "戦")

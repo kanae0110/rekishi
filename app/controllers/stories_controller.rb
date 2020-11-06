7b30c8cc1d3e9f0bb222643749a3c5595a5e6786
EC2 Default User <ec2-user@ip-172-31-33-217.us-east-2.compute.internal>
2020-11-06 06:33:09 +0000

    リンクつけ途中

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -2,8 +2,12 @@ class StoriesController < ApplicationController
     
   def index
       @stories = Story.all
-     
+       @stories = @stories.where(category: "人物")
+      
   end  
     
+  def show
+    
+  end
     
 end

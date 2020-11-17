f05655eb01555576541c809dc63bc3f3ccafa970
kanae Shingaki <mmm.syh@gmail.com>
2020-11-17 13:31:11 +0000

    edit編集

--- app/controllers/admin/quizzes_controller.rb
+++ app/controllers/admin/quizzes_controller.rb
@@ -30,13 +30,13 @@ class Admin::QuizzesController < ApplicationController
         
     def edit
         @quiz = Quiz.find(params[:id])
-        @story = Story.find(params[:story_id])
-　  end
+        
+    end
     
     def update
         @quiz = Quiz.find(params[:id])
        
-        quiz_params = params.require(:quiz).permit(:question, :answer1, :answer2, :answer3, :answer, :discription, :story_id)
+        quiz_params = params.require(:quiz).permit(:question, :answer1, :answer2, :answer3, :answer, :discription, :story_id, :content)
         if @quiz.update(quiz_params)
             redirect_to admin_quizzes_path
         else

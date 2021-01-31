edb10a3d44982dd00c59972fc03490d034db19a5
kanae Shingaki <mmm.syh@gmail.com>
2021-01-31 12:31:44 +0000

    ユーザーデータ追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@ Story.create(name: "龍馬の生い立ち", category: "人物")
 Story.create(name: "とある貴族の幕末京都", category: "人物")
 Story.create(name: "天誅", category: "事件")
 Story.create(name: "京の戦い", category: "戦")
+
+User.create(name: "ゲスト1", email: "guest1@example.com", password: "password", password_confirmation: "password")
+User.create(name: "ゲスト2", email: "guest2@example.com", password: "password", password_confirmation: "password")
+User.create(name: "ゲスト3", email: "guest3@example.com", password: "password", password_confirmation: "password")
